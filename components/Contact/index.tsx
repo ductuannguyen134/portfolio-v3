616c7e7a321ccf722abcc241814005bc18da6c21
Chris Nguyen <ductuan1999@gmail.com>
2021-09-16 15:35:55 +1000

    update

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -6,43 +6,37 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Link from "next/link";
 import Modal from "../Modal";
+import Card from "../Card";
 
 const Contacts = () => {
-  const [visibleModal, setVisibleModal] = useState(false);
-  const [currentImages, setCurrentImages] = useState<string[]>([]);
   return (
-    <Container id="nav-5" title="Contact">
-      <div
-        style={{
-          backgroundColor: "white",
-          color: "#292631",
-          borderRadius: "10px",
-          padding: 20,
-        }}
-      >
-        <h3>Email: ductuan1999@gmail.com</h3>{" "}
-        <h3>
-          GitHub:{" "}
-          <a
-            href="https://github.com/ductuannguyen134"
-            target="_blank"
-            rel="noreferrer"
-            style={{ textDecoration: "none" }}
-          >
-            https://github.com/ductuannguyen134
-          </a>
-        </h3>
-        <h3>
-          LinkedIn:{" "}
-          <a
-            href="https://www.linkedin.com/in/duc-tuan-nguyen-7914a317a/"
-            style={{ textDecoration: "none" }}
-            target="_blank"
-            rel="noreferrer"
-          >
-            https://www.linkedin.com/in/duc-tuan-nguyen-7914a317a/
-          </a>
-        </h3>
+    <Container id="nav-5" title="Contacts">
+      <div style={{ flex: "50%" }}>
+        <Card>
+          <p>Email: ductuan1999@gmail.com</p>
+          <p>
+            GitHub:{" "}
+            <a
+              href="https://github.com/ductuannguyen134"
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: "underline", color: "white" }}
+            >
+              https://github.com/ductuannguyen134
+            </a>
+          </p>
+          <p>
+            LinkedIn:{" "}
+            <a
+              href="https://www.linkedin.com/in/duc-tuan-nguyen-7914a317a/"
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: "underline", color: "white" }}
+            >
+              https://www.linkedin.com/in/duc-tuan-nguyen-7914a317a/{" "}
+            </a>
+          </p>
+        </Card>
       </div>
     </Container>
   );
